492b4a1d3e15a434979854a15cef2331c1b28772
natelee7csuglobal <nathanial.anderson@csuglobal.edu>
2025-03-09 19:08:31 -0500

    Module 4

---
+++ Cone.java
@@ -0,0 +1,27 @@
+package module4;
+
+public class Cone extends Shape {
+    private double radius;
+    private double height;
+
+    public Cone(double radius, double height) {
+        this.radius = radius;
+        this.height = height;
+    }
+
+    @Override
+    public double surface_area() {
+        double slantHeight = Math.sqrt(radius * radius + height * height);
+        return Math.PI * radius * (radius + slantHeight);
+    }
+
+    @Override
+    public double volume() {
+        return (1.0 / 3.0) * Math.PI * radius * radius * height;
+    }
+
+    @Override
+    public String toString() {
+        return "Cone - Surface Area: " + surface_area() + ", Volume: " + volume();
+    }
+}
---
+++ ShapeArray.java
@@ -0,0 +1,15 @@
+package module4;
+
+public class ShapeArray {
+    public static void main(String[] args) {
+        Shape[] shapeArray = new Shape[3];
+
+        shapeArray[0] = new Sphere(5.0);
+        shapeArray[1] = new Cylinder(3.0, 7.0);
+        shapeArray[2] = new Cone(4.0, 6.0);
+
+        for (Shape shape : shapeArray) {
+            System.out.println(shape);
+        }
+    }
+}
